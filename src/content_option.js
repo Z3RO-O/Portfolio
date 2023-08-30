13f719d1d5a9b6292868f78d66583a290430744f
Z3RO-O <vaibhavsingh_21523@aitpune.edu.in>
2023-08-30 11:24:34 +0530

    convertedToVite

--- src/content_option.js
+++ src/content_option.js
@@ -1,8 +1,17 @@
+import one from "./assets/svg/projects/one.svg";
+import two from "./assets/svg/projects/two.svg";
+import three from "./assets/svg/projects/three.svg";
+import four from "./assets/svg/projects/four.svg";
+import five from "./assets/svg/projects/five.svg";
+import six from "./assets/svg/projects/six.svg";
+import seven from "./assets/svg/projects/seven.svg";
+import eight from "./assets/svg/projects/eight.svg";
+
 const logotext = "VAIBHAV";
 const meta = {
   title: "Vaibhav Singh",
   description:
-    "I’m Vaibhav Singh Programmer _ Web devloper,currently working in Trialshopy",
+    "I’m Vaibhav Singh | A Programmer, Web devloper,currently working in Trialshopy",
 };
 
 const introdata = {
@@ -94,25 +103,102 @@ const services = [
   },
 ];
 
-const dataportfolio = [
+const projects = [
   {
-    img: "https://picsum.photos/400/?grayscale",
-    description:
-      "The wisdom of life consists in the elimination of non-essentials.",
-    link: "#",
+    id: 1,
+    projectName: "LSB Steganography",
+    projectDesc:
+      "This project aims to build a Website which can do steganography in all type of files.",
+    tags: ["React", "Tailwind CSS", "Flask"],
+    code: "https://github.com/Z3RO-O/LSB-Steganography",
+    demo: "https://github.com/Z3RO-O/LSB-Steganography",
+    image: one,
   },
   {
-    img: "https://picsum.photos/400/300/?grayscale",
-    description:
-      "The wisdom of life consists in the elimination of non-essentials.",
-    link: "#",
+    id: 2,
+    projectName: "Railway Tracking",
+    projectDesc:
+      "This project is a railway tracking system that allows users to obtain relevant information about train timing.",
+    tags: ["Django", "Tailwind CSS"],
+    code: "https://github.com/hhhrrrttt222111/developer-portfolio",
+    demo: "https://github.com/hhhrrrttt222111/developer-portfolio",
+    image: two,
   },
   {
-    img: "https://picsum.photos/400/?grayscale",
-    description:
-      "The wisdom of life consists in the elimination of non-essentials.",
-    link: "#",
+    id: 3,
+    projectName: "Weather App",
+    projectDesc:
+      "Weather forecast systems and applications predict weather conditions based on multiple parameters.",
+    tags: ["Django", "CSS", "Material Ui"],
+    code: "https://github.com/hhhrrrttt222111/developer-portfolio",
+    demo: "https://github.com/hhhrrrttt222111/developer-portfolio",
+    image: three,
   },
+  {
+    id: 4,
+    projectName: "Android Patient Tracker",
+    projectDesc:
+      "This project involves the development of an Android application for viewing and managing patient data.",
+    tags: ["Flutter", "Firebase"],
+    code: "https://github.com/hhhrrrttt222111/developer-portfolio",
+    demo: "https://github.com/hhhrrrttt222111/developer-portfolio",
+    image: four,
+  },
+  {
+    id: 5,
+    projectName: "E-Commerce App",
+    projectDesc: "A Simple E-commerce application",
+    tags: ["React Native", "Firebase"],
+    code: "https://github.com/hhhrrrttt222111/developer-portfolio",
+    demo: "https://github.com/hhhrrrttt222111/developer-portfolio",
+    image: five,
+  },
+  {
+    id: 6,
+    projectName: "Uber Lite",
+    projectDesc: "Uber clone",
+    tags: ["Flutter"],
+    code: "https://github.com/hhhrrrttt222111/developer-portfolio",
+    demo: "https://github.com/hhhrrrttt222111/developer-portfolio",
+    image: six,
+  },
+  {
+    id: 7,
+    projectName: "Stock Market App",
+    projectDesc: "A simple stock market API app",
+    tags: ["React", "Redux", "Bootstrap"],
+    code: "https://github.com/hhhrrrttt222111/developer-portfolio",
+    demo: "https://github.com/hhhrrrttt222111/developer-portfolio",
+    image: seven,
+  },
+  {
+    id: 8,
+    projectName: "Car Pooling System",
+    projectDesc:
+      "The carpooling system merges multiple people in a car which leads to meet new people, reduces pollution",
+    tags: ["Flutter", "React"],
+    code: "https://github.com/hhhrrrttt222111/developer-portfolio",
+    demo: "https://github.com/hhhrrrttt222111/developer-portfolio",
+    image: eight,
+  },
+  // {
+  //   img: "https://picsum.photos/400/?grayscale",
+  //   description:
+  //     "The wisdom of life consists in the elimination of non-essentials.",
+  //   link: "#",
+  // },
+  // {
+  //   img: "https://picsum.photos/400/300/?grayscale",
+  //   description:
+  //     "The wisdom of life consists in the elimination of non-essentials.",
+  //   link: "#",
+  // },
+  // {
+  //   img: "https://picsum.photos/400/?grayscale",
+  //   description:
+  //     "The wisdom of life consists in the elimination of non-essentials.",
+  //   link: "#",
+  // },
 ];
 
 const contactConfig = {
@@ -137,7 +223,7 @@ const socialprofils = {
 export {
   meta,
   dataabout,
-  dataportfolio,
+  projects,
   worktimeline,
   skills,
   services,
---
+++ src/pages/projects/index.jsx
@@ -0,0 +1,41 @@
+import "./style.css";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { Container, Row, Col } from "react-bootstrap";
+import { meta,projects} from "../../content_option";
+import SingleProject from "./SingleProject";
+
+export const Projects = () => {
+  return (
+    <HelmetProvider>
+      <Container className="About-header">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title> Projects | {meta.title} </title>{" "}
+          <meta name="description" content={meta.description} />
+        </Helmet>
+        <Row className="mb-5 mt-3 pt-md-3">
+          <Col lg="8">
+            <h1 className="display-4 mb-4"> Projects </h1>{" "}
+            <hr className="t_border my-4 ml-0 text-left" />
+          </Col>
+        </Row>
+        <div className="mb-5 po_items_ho">
+          {projects.map((project) => {
+            return (
+              <SingleProject
+                key={project.id}
+                id={project.id}
+                name={project.projectName}
+                desc={project.projectDesc}
+                tags={project.tags}
+                code={project.code}
+                demo={project.demo}
+                image={project.image}
+              />
+            );
+          })}
+        </div>
+      </Container>
+    </HelmetProvider>
+  );
+};
